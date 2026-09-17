2efc15262b40fcf41acccfb2f37b5d62e60619b0
XimicYT <abutts29@students.unorth.k12.in.us>
2026-09-17 15:29:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
 
 const app = express();
-app.use(cors());
-
 const server = http.createServer(app);
+
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] }
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
 });
+
+app.use(express.static('public'));
 
 const players = {};
 
 io.on('connection', (socket) => {
-  console.log(`[CONNECT] Player joined: ${socket.id}`);
+  players[socket.id] = { id: socket.id, x: 0, y: 0, mapGroup: 0, mapNum: 0 };
 
   socket.on('updatePosition', (data) => {
-    players[socket.id] = {
-      x: data.x,
-      y: data.y,
-      mapId: data.mapId,
-      lastSeen: Date.now()
-    };
-    
-    // Send updated player state to ALL connected clients
-    io.emit('playerUpdates', players);
+    if (players[socket.id]) {
+      players[socket.id] = { ...players[socket.id], ...data, id: socket.id };
+      socket.broadcast.emit('playerMoved', players[socket.id]);
+    }
   });
 
   socket.on('disconnect', () => {
-    console.log(`[DISCONNECT] Player left: ${socket.id}`);
     delete players[socket.id];
     io.emit('playerDisconnected', socket.id);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log(`Netplay server active on port ${PORT}`);
+});
