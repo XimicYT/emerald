570f98b6639c02b851271b8b211fa68f3d99f64d
XimicYT <abutts29@students.unorth.k12.in.us>
2026-09-19 11:55:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,88 +1,46 @@
 const express = require('express');
-
 const http = require('http');
-
 const { Server } = require('socket.io');
 
-
-
 const app = express();
-
 const server = http.createServer(app);
 
-
-
 const io = new Server(server, {
-
   cors: {
-
     origin: '*',
-
     methods: ['GET', 'POST']
-
   }
-
 });
-
-
 
 app.use(express.static('public'));
 
-
-
 const players = {};
 
-
-
 io.on('connection', (socket) => {
-
-  players[socket.id] = { id: socket.id, x: 0, y: 0, mapGroup: 0, mapNum: 0, dir: 1 };
-
-
+  // Added gender: 0 to default player data
+  players[socket.id] = { id: socket.id, x: 0, y: 0, mapGroup: 0, mapNum: 0, dir: 1, gender: 0 };
 
   // Send all active players to the newly connected user
-
   socket.emit('currentPlayers', players);
 
-
-
   // Notify everyone else about the new user
-
   socket.broadcast.emit('playerJoined', players[socket.id]);
 
-
-
   socket.on('updatePosition', (data) => {
-
     if (players[socket.id]) {
-
+      // Automatically merges incoming gender data along with x, y, dir, mapGroup, etc.
       players[socket.id] = { ...players[socket.id], ...data, id: socket.id };
-
       socket.broadcast.emit('playerMoved', players[socket.id]);
-
     }
-
   });
-
-
 
   socket.on('disconnect', () => {
-
     delete players[socket.id];
-
     io.emit('playerDisconnected', socket.id);
-
   });
-
 });
 
-
-
 const PORT = process.env.PORT || 10000;
-
 server.listen(PORT, () => {
-
   console.log(`Netplay server active on port ${PORT}`);
-
-}); 
-
+});
